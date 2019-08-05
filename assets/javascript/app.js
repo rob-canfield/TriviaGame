be85fb35fd34748eb2c438f3cd58ece39a515fa1
Rob Canfield <git@livesenjoyable.com>
2019-08-05 12:53:57 -0600

    added questions object

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+//questions answers into object
+var questions = [{
+        question : "Where was Link born?",
+        answers : ["Kokiri Forest", "Hyrule", "Gerudu Valley", "Kokiriko Village"],
+        correct : 2
+    }, {
+        question : "Where did Link learn to play the 'Song of Storms?'",
+        answers : ["Lake Hyrule", "Death Mountain", "Inside the Windmill", "Desert Wasteland"],
+        correct : 3
+    }, {
+        question : "Which year was Ocarina of Time released?",
+        answers : ["1998", "1992", "2000", "1995"],
+        correct : 1
+    }, {
+        question : "How many years did Link remain in the Spiritual Realm after drawing the Master Sword?",
+        answers : ["fourteen", "seven", "four", "twenty"],
+        correct : 2
+    }, {
+        question : "After Link found and killed all 100 Gold Skultas, what was his reward?",
+        answers : ["Giant's Wallet", "Golden Scale", "200 rupees", "Golden Gauntlets"],
+        correct : 3
+    }, {
+        question : "What special weapon does Link need to use to defeat Ganon at the end of the game?",
+        answers : ["Arrows of Light", "Goron Hammer", "Bombshoes", "Faore's Wind"],
+        correct : 1
+    }, {
+        question : "What gift does Princess Zelda give to Link when they first meet?",
+        answers : ["The Ocarina of Time", "a hand written letter", "Zelda's Lullaby", "a new hat"],
+        correct : 2
+    }, {
+        question : "What number installment is Ocarina of Time in The Legend of Zelda Series",
+        answers : ["first", "second", "fifth", "fourth"],
+        correct : 3
+    }, {
+        question : "Which TV series was Ocarina of Time inspired by?",
+        answers : ["Twin Peaks", "Buffy the Vampire Slayer", "On the Air", "The X-Files"],
+        correct : 1
+    }, {
+        question : "What was the name of the Graveyard keeper?",
+        answers : ["Milo", "Ingo", "Grog", "Dampe"],
+        correct : 4
+    }
+]
